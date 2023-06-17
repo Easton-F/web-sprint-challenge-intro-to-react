d224bb7d8670ce10edd453014f698b7ee3bb2d12
Easton <easton.fritsch21@gmail.com>
2023-06-17 16:24:23 -0600

    added hooks useEffect and useState and implemented them in App.js as well as set up Character component in Character.js. As of now only renders Luke Skywalker. Passing 2/7 tests

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
+import React from "react";
+
 // Write your Character component here
+const Character = (props) => {
+
+    console.log('The Characters', props.character);
+
+    for (let i = 0; i < props.character.length; i++){
+    return (
+        <div className = "character-section-wrapper">
+            <h2>{props.character[i].name}</h2>
+            <p>{props.character[i].birth_year}</p>
+        </div>
+    )
+  }
+}
+
+export default Character;
